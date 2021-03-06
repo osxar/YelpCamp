8cc35b3f6c50ef87e96a888d3e4959d736db8ddc
oscar <55120346+osxar@users.noreply.github.com>
2021-03-06 11:14:24 -0500

    Add files via upload
    Updates

--- middleware.js
+++ middleware.js
@@ -38,7 +38,7 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'It seems you don\'t have permission!');
+        req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
@@ -48,7 +48,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'It seems you don\'t have permission!'); 
+        req.flash('error', 'You do not have permission to do that!'); 
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
